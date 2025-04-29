1dcef14cb5339640e5ab0ab3c5e3988c3834da2f
鲨鱼辣椒 <monishine_code@163.com>
2025-04-29 11:30:54 +0800

    chore(layout): 调整Content组件

--- components/layout/Content/index.tsx
+++ components/layout/Content/index.tsx
@@ -1,20 +1,7 @@
-'use client';
-
 import React from 'react';
-import { useTheme } from 'next-themes';
 
 function Content({ children }: React.PropsWithChildren) {
-	const { theme } = useTheme();
-
-	return (
-		<main
-			className={`${
-				theme === 'dark' ? 'dark' : 'light'
-			} text-foreground bg-background lg:w-[1800px] m-4`}
-		>
-			{children}
-		</main>
-	);
+	return <main className='lg:w-[1800px] m-4'>{children}</main>;
 }
 
 export default Content;

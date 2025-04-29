645b5709f2de4f77de466a341904ea02c290a1cb
鲨鱼辣椒 <monishine_code@163.com>
2025-04-29 09:50:38 +0800

    feat(): 添加暗黑主题适配

--- components/layout/Content/index.tsx
+++ components/layout/Content/index.tsx
@@ -1,9 +1,20 @@
-interface ContentProps {
-	children: Readonly<React.ReactNode>;
-}
+'use client';
 
-function Content({ children }: ContentProps) {
-	return <main className='dark text-foreground bg-background'>{children}</main>;
+import React from 'react';
+import { useTheme } from 'next-themes';
+
+function Content({ children }: React.PropsWithChildren) {
+	const { theme } = useTheme();
+
+	return (
+		<main
+			className={`${
+				theme === 'dark' ? 'dark' : 'light'
+			} text-foreground bg-background lg:w-[1800px] m-4`}
+		>
+			{children}
+		</main>
+	);
 }
 
 export default Content;

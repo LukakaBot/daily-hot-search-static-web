cc5e70699a97b9df5e415961e02ff04948cd0740
鲨鱼辣椒 <monishine_code@163.com>
2025-04-29 16:17:50 +0800

    feat(layout): 调整页头

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,12 +1,47 @@
 'use client';
 
-import ThemeSwitch from '@/components/theme/ThemeSwitch';
+import { useTheme } from 'next-themes';
+import { Sun, Moon } from 'lucide-react';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import BaseIcon from '@/components/Base/Icon';
 
 function Header() {
+	const { setTheme } = useTheme();
+
 	return (
-		<header className='flex justify-center items-center mx-auto px-2 rounded-md'>
-			<h1>我是头部</h1>
-			<ThemeSwitch />
+		<header className='border-b-border sticky top-0 z-30 border-b border-dashed'>
+			<div className='container-wrapper flex justify-between items-center gap-2'>
+				<div className='flex-1 flex gap-2 items-center'>
+					<BaseIcon name='icon-[bxs--hot]' color='red' />
+					<div>每日热搜</div>
+				</div>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant='outline' size='icon'>
+							<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+							<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+							<span className='sr-only'>Toggle theme</span>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align='end'>
+						<DropdownMenuItem onClick={() => setTheme('light')}>
+							Light
+						</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setTheme('dark')}>
+							Dark
+						</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setTheme('system')}>
+							System
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
 		</header>
 	);
 }

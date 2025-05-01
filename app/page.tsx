627cda08c6bf68f2663dc62bd63387a539c3c763
鲨鱼辣椒 <monishine_code@163.com>
2025-05-01 21:19:01 +0800

    fix: 修复首页热搜标题超出部分文本隐藏问题

--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,110 @@
-'use client';
+"use client";
 
-import { fetchWeiboHotSearchList } from '@/api/hotSearch';
-import { useEffect, useState } from 'react';
-import { HotSearchDataItem } from '@/api/common/types';
-import { useLoading } from '@/hook';
-import BaseIcon from '@/components/Base/Icon';
+import { fetchWeiboHotSearchList } from "@/api/hotSearch";
+import { useEffect, useState } from "react";
+import { HotSearchDataItem } from "@/api/common/types";
+import { useLoading } from "@/hook";
+import BaseIcon from "@/components/Base/Icon";
 import {
-	Card,
-	CardContent,
-	CardHeader,
-	CardTitle,
-	CardDescription,
-} from '@/components/ui/card';
-import { ScrollArea } from '@/components/ui/scroll-area';
-import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Badge } from "@/components/ui/badge";
+// import { Button } from "@/components/ui/button";
 
 function getBadgeColorClassName(index: number) {
-	if (index <= 0) {
-		return 'bg-red-600 text-white';
-	} else if (index <= 1) {
-		return 'bg-red-500 text-white';
-	} else if (index <= 2) {
-		return 'bg-red-400 text-white';
-	}
+  if (index <= 0) {
+    return "bg-red-600 text-white";
+  } else if (index <= 1) {
+    return "bg-red-500 text-white";
+  } else if (index <= 2) {
+    return "bg-red-400 text-white";
+  }
 }
 
 function Home() {
-	const [weiboHotSearchList, setWeiboHotSearchList] = useState<
-		HotSearchDataItem[]
-	>([]);
-	const [loading, setLoading] = useLoading();
+  const [weiboHotSearchList, setWeiboHotSearchList] = useState<
+    HotSearchDataItem[]
+  >([]);
+  const [loading, setLoading] = useLoading();
 
-	const getWeiboData = async () => {
-		try {
-			setLoading(true);
-			const data = await fetchWeiboHotSearchList();
-			setWeiboHotSearchList(data);
-		} finally {
-			setLoading(false);
-		}
-	};
+  const getWeiboData = async () => {
+    try {
+      setLoading(true);
+      const data = await fetchWeiboHotSearchList();
+      setWeiboHotSearchList(data);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	useEffect(() => {
-		getWeiboData();
-	}, []);
+  useEffect(() => {
+    getWeiboData();
+  }, []);
 
-	return (
-		<div className='w-full'>
-			<div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-				<Card>
-					<CardHeader>
-						<CardTitle>
-							<div className='flex'>
-								<BaseIcon className='mr-2' name='svg-xinlang' />
-								微博
-							</div>
-						</CardTitle>
-						<CardDescription>123</CardDescription>
-					</CardHeader>
-					<CardContent>
-						<ScrollArea className='h-72 rounded-md'>
-							{loading ? (
-								<div className='flex justify-center items-center h-72'>
-									<span className='loading loading-infinity loading-xl'></span>
-								</div>
-							) : (
-								weiboHotSearchList.map((item, index) => {
-									return (
-										<div
-											className='flex items-center not-last:mb-2 cursor-pointer'
-											key={item.id}
-										>
-											<Badge
-												className={getBadgeColorClassName(index)}
-												variant='secondary'
-											>
-												{item.id}
-											</Badge>
-											<Button
-												variant='link'
-												onClick={() => window.open(item.url, '_blank')}
-											>
-												{item.title}
-											</Button>
-										</div>
-									);
-								})
-							)}
-						</ScrollArea>
-					</CardContent>
-				</Card>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              <div className="flex">
+                <BaseIcon className="mr-2" name="svg-xinlang" />
+                微博
+              </div>
+            </CardTitle>
+            <CardDescription>123</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ScrollArea className="h-72 rounded-md">
+              {loading ? (
+                <div className="flex justify-center items-center h-72">
+                  <span className="loading loading-infinity loading-xl"></span>
+                </div>
+              ) : (
+                weiboHotSearchList.map((item, index) => {
+                  return (
+                    <div
+                      className="w-full flex items-center not-last:mb-2 cursor-pointer"
+                      key={item.id}
+                    >
+                      <Badge
+                        className={getBadgeColorClassName(index)}
+                        variant="secondary"
+                      >
+                        {item.id}
+                      </Badge>
+                      {/* <div>
+                        <Button
+                          // className='overflow-hidden whitespace-normal break-words'
+                          className="whitespace-normal"
+                          variant="link"
+                          onClick={() => window.open(item.url, "_blank")}
+                        >
+                          {item.title}
+                        </Button>
+                      </div> */}
+                      <span
+                        className="inline-flex items-center justify-center gap-2 px-4 py-2 rounded-md text-sm font-medium underline-offset-4 hover:underline"
+                        onClick={() => window.open(item.url, "_blank")}
+                      >
+                        {item.title}
+                      </span>
+                    </div>
+                  );
+                })
+              )}
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
